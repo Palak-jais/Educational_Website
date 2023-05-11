ee88a7e4de8786629db578a68011f9b3d24f9859
palak jaiswal <118873142+Palak-jais@users.noreply.github.com>
2023-05-11 20:32:04 +0530

    Add files via upload
    A educational website

---
+++ type.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings:[" WE provide Quality Content "," Best tutors and experts"," We provide Best Documentation "],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:70,
+    loop:true
+});
